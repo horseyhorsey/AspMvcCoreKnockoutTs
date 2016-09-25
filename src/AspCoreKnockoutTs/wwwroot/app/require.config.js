68093ff000c5fbb151ee5fe683667b6fd7d73e2f
dave <dub.cyclone@gmail.com>
2016-09-25 21:00:55 +0100

    Removed cdn requireJS from production tags. Add tether script tag above existing scripts.

--- src/AspCoreKnockoutTs/wwwroot/app/require.config.js
+++ src/AspCoreKnockoutTs/wwwroot/app/require.config.js
@@ -8,11 +8,11 @@ var require = {
         "jquery": "lib/jquery/dist/jquery",
         "knockout": "lib/knockout/dist/knockout",
         "knockout-projections": "lib/knockout-projections/dist/knockout-projections",
-        "signals": "lib/js-signals/dist/signals.min",        
+        "signals": "lib/js-signals/dist/signals.min",
         "text": "lib/requirejs-text/text"
     }
     ,
     shim: {
-        "bootstrap": { deps: ["tether","jquery"] }
+        "bootstrap": { deps: ["tether", "jquery"] }
     }
 };
